bc9c4dd9b6e062b2491b4c278e16986de68c4400
Samu András <samu.andris1@gmail.com>
2017-10-21 20:07:49 +0200

    Body weight predictor added

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # DeepLearingLab
 
 # Projects in this git:
+* Body weight predictor: Predicts body weight with linear regression from the brain weight
 * Small script, which can tell apart loud and quiet sounds and also high pitch and low pitch sounds live 
 * Sentence generator (using markov chains)
--- animalweight.py
+++ animalweight.py
@@ -1,6 +1,18 @@
 import pandas as pd
 from sklearn import linear_model
-import matplotlib.pyplot as plt
+import warnings
+
+warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 
 # read data:
-dataframe = pd.read_fwf("brain_body.txt")
+dataframe = pd.read_fwf('brain_body.txt')
+x_values = dataframe[['Brain']]
+y_values = dataframe[['Body']]
+
+# train model on data:
+body_reg = linear_model.LinearRegression()
+body_reg.fit(x_values, y_values)
+
+# visualise result:
+brainWeight = 3.5
+print('For ', brainWeight, ' kg brain the body weight is: ', body_reg.predict(brainWeight)[0][0], ' kg')
